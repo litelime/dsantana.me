66ea5e0dd662886836a8d7f4dc8670d9791c0f19
David <larrysyss@gmail.com>
2017-10-19 09:23:28 -0700

    laptop
    Former-commit-id: 537a14c601b2d970646a3baf36f22d6e05da26b9

--- image_site/index.php
+++ image_site/index.php
@@ -6,6 +6,7 @@
 		<link href="./image.css?ver=1.0" rel="stylesheet" type="text/css" /> 
 		<script type="text/javascript" src="./prototype.js"></script>
 		<script type="text/javascript" src="./image.js?ver=1.0"></script>
+		<meta name="viewport" content="width=device-width, initial-scale=1">
 
 	</head>
 
